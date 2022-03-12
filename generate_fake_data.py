a358a9e3ddc9961ecf6198b3f2586d96b624fda0
Raoul Hasdell <98950750+rlh19@users.noreply.github.com>
2022-03-12 19:18:12 +0000

    Update generate_fake_data.py

--- generate_fake_data.py
+++ generate_fake_data.py
@@ -5,10 +5,8 @@ import numpy as np
 from astropy.table import Table
 import astropy.constants as const
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['text.usetex'] = True
-mpl.rcParams['font.family'] = 'serif'
-mpl.rcParams['font.size'] = 11
+import scipy.special
+import math
 
 def get_oscillation_array(M, R, l_max, n_max):
     oscillation_array = np.zeros((n_max+1, l_max+1))
@@ -23,6 +21,12 @@ def lorentzian(x, x_0, width):
 
 def gaussian(x, sigma, mu):
     return np.exp(-(x - mu)**2 / (2 * sigma**2))
+
+def nu_r(m, nu, omega):
+    return nu + m*omega
+
+def ep(m, l, i):
+    return math.factorial(l - abs(m)) / math.factorial(l + abs(m)) * (scipy.special.lpmn(m, l, np.cos(i))[0][abs(m), l])**2
 
 G = const.G.value
 L_sun = const.L_sun.value # W
@@ -45,32 +49,34 @@ t = Table.read(filename)
 print('Would you like to run through all stars in the table or select one at random?')
 while True:
     selection = input('[all/id/random/sun] >>> ')
-    if selection == 'all':
+    if selection.lower() == 'all':
         IDs = t['ID']
         break
-    if selection == 'id':
+    if selection.lower() == 'id':
         print('To add a star to the list, input its ID and press ENTER. When you are finished, press ENTER again:')
         IDs = []
         while True:
             query = input('>>> ')
             if query == '':
                 break
-            elif np.count_nonzero(t['ID'] == int(query)) == 1:
+            elif query.isdigit() and np.count_nonzero(t['ID'] == int(query)) == 1:
                 IDs.append(int(query))
                 print('Added to list')
             else:
                 print('Star not found')
         break
-    if selection == 'random':
+    if selection.lower() == 'random':
         IDs = [np.random.choice(t['ID'])]
         break
-    if selection == 'sun':
+    if selection.lower() == 'sun':
         IDs = ['Sun']
         L = L_sun
         M = M_sun
         R = R_sun
         g = g_solar
         T_eff = T_eff_sun
+        inclination = 0
+        angular_freq = (2e6 * np.pi) / (26.24 * 24 * 3600)
         break
 for ID in IDs:
     print('Now generating: {}'.format(ID))
@@ -81,6 +87,8 @@ for ID in IDs:
         R = float(np.sqrt((G * (t['Mass'][index] * M_sun)) / ((10**t['logg'][index]) * 0.01)))
         g = float(10**t['logg'][index] * 0.01)
         T_eff = float(10**t['logTe'][index])
+        inclination = float(t['inclination'][index])
+        angular_freq = float(t['angular_freq'][index])
     oscillation_array = get_oscillation_array(M, R, 3, 39)
     T_red = 8907 * (L / L_sun)**-0.093 # K
     beta = (1 - np.exp((T_eff - T_red) / delta_T))
@@ -93,47 +101,46 @@ for ID in IDs:
         print('WARNING! This star has oscillations exceeding the observational frequency limit. Would you like to simulate using the fast camera instead? [Note: This will generate 10 times more data!]')
         while True:
             warning = input('[y/n] >>> ')
-            if warning == 'y':
+            if warning.lower() == 'y':
                 cadence = 2.5 # s
                 break
-            if warning == 'n':
+            if warning.lower() == 'n':
                 break
     lower_limit = 1e6 / duration # μHz
     upper_limit = 1e6 / (2 * cadence) # μHz
     x = np.linspace(lower_limit, upper_limit, round(upper_limit / lower_limit - 1)) # μHz
-    y = nu_max / nu_max_sun
-    sigma_c = sigma_c_sun * (L / L_sun) * ((M_sun / M)**1.5) * ((T_eff_sun / T_eff)**2.75) * np.sqrt(y)
-    tau_c = tau_c_sun / y # Ms
+    z = nu_max / nu_max_sun
+    sigma_c = sigma_c_sun * (L / L_sun) * ((M_sun / M)**1.5) * ((T_eff_sun / T_eff)**2.75) * np.sqrt(z)
+    tau_c = tau_c_sun / z # Ms
     P_granulation = 4 * sigma_c**2 * tau_c / (1 + (2 * np.pi * x * tau_c)**2)
-    delta_gamma_dip = -0.47 * y + 0.62
-    Wdip = 4637 * y - 141
-    nu_dip = 2984 * y + 60 
-    plt.figure(figsize=(5.5, 4))
+    delta_gamma_dip = -0.47 * z + 0.62
+    Wdip = 4637 * z - 141
+    nu_dip = 2984 * z + 60
+    if nu_max < 3900:
+        alpha = 2.95 * z + 0.39
+        gamma_alpha = 3.08 * z + 3.32
+        linewidth = np.exp((alpha*np.log(oscillation_array/nu_max) + np.log(gamma_alpha)) + ((np.log(delta_gamma_dip))/(1+(((2*np.log(oscillation_array/nu_dip))/np.log(Wdip/nu_max))**2))))
+    else:
+        alpha = 1.6
+        gamma_alpha = 1.38
+        linewidth = np.exp((alpha*np.log(oscillation_array/nu_max) + np.log(gamma_alpha)))
+    plt.figure(figsize=(7, 5))
     y_combined = np.zeros_like(x)
     for i in range(oscillation_array.shape[1]):
         y = np.zeros_like(x)
+        m = np.arange(-i, i+1)
         for j in range(oscillation_array.shape[0]):
-            if nu_max < 3900:
-                alpha = 2.95 * y + 0.39
-                gamma_alpha = 3.08 * y + 3.32
-                linewidth = np.exp((alpha*np.log(oscillation_array[j, i]/nu_max) + np.log(gamma_alpha)) + ((np.log(delta_gamma_dip))/(1+(((2*np.log(oscillation_array[j, i]/nu_dip))/np.log(Wdip/nu_max))**2))))
-            else:
-                alpha = 1.6
-                gamma_alpha = 1.38
-                linewidth = np.exp((alpha*np.log(oscillation_array[j, i]/nu_max) + np.log(gamma_alpha)))
-            y += gaussian(x, sigma, nu_max) * amplitude**2 * 2 / np.pi / linewidth * visibility[i] * lorentzian(x, oscillation_array[j, i], linewidth)
+            for k in m:
+                y += gaussian(x, sigma, nu_max) * amplitude**2 * 2 / np.pi / linewidth[j, i] * visibility[i] * lorentzian(x, nu_r(k, oscillation_array[j, i], angular_freq), linewidth[j, i]) * ep(k, i, inclination)
         plt.plot(x, y + P_granulation, label=r'$l$ = {}'.format(i))
         y_combined += y
-        if i == 1:
-            plot_lim_index = np.argmax(y)
-            plot_lim = np.max(y)
     plt.axvline(x=nu_max, ls='--', color='gray', label=r'$\nu_{\rm max} =$'+' {} '.format(round(nu_max))+r'$\mu$Hz')
     plt.plot(x, P_granulation, color='k', label='Granulation')
     plt.title('Stellar oscillation spectrum [ID: {}]'.format(ID))
     plt.xlabel(r'Frequency, $\nu$ [$\mu$Hz]')
     plt.ylabel(r'PSD [ppm$^2 \mu$Hz$^{-1}$]')
-    plt.legend()
-    plt.xlim(nu_max-3000, nu_max+2000)
-    plt.ylim(0, 1.1*(P_granulation[plot_lim_index] + plot_lim))
+    plt.legend(loc='upper right')
+    plt.xlim(nu_max - 2 * fwhm, nu_max + 2 * fwhm)
+    plt.ylim(0, 1.1 * np.max((y_combined + P_granulation)[np.abs(x - nu_max) < 2 * fwhm]))
     plt.savefig('{}.pdf'.format(ID))
     np.save('{}.npy'.format(ID), np.vstack((x, y_combined + P_granulation)).T)
